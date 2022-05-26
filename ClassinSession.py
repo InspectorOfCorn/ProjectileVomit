1cd6c4c88ef87a7581b5a5133ce3ed6f8205982a
Bilal Omar <bilal.m.omar@gmail.com>
2022-05-26 15:57:48 -0700

    COMMIT

--- ClassinSession.py
+++ ClassinSession.py
@@ -1,21 +1,41 @@
 
-
-
 class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + "@company.com"
+        
+        Employee.num_of_emps += 1
 
     def fullname(self):
         return '{} {}' .format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+
+
 #Classes are blueprints for creating instances and each unique employee we create with our calss they each become instances
 
 emp_1 = Employee('Test', 'User', 50000)
 emp_2 = Employee('Bilal', 'Omar', 200000)
 
-print(Employee.fullname(emp_1))
-# print(emp_2.fullname())
-# Remember () after function 
+Employee.set_raise_amt(1.05)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
+
+# print(emp_1.__dict__)
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
